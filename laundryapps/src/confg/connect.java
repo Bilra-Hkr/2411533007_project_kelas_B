b2825c901c3ecb41da7a80ec6bcab58095550758
Bilra-Hkr <derielchae@gmail.com>
2025-09-14 18:23:55 +0700

    Add files via upload

---
+++ laundryapps/src/confg/connect.java
@@ -0,0 +1,13 @@
+package confg;
+
+public class connect {
+    public static void main(String[] args) {
+//        if (Database.koneksi() != null) {
+//            System.out.println("Koneksi berhasil ke database!");
+//        } else {
+//            System.out.println("Koneksi gagal!");
+//        }
+    	Database.koneksi();
+    }
+}
+
