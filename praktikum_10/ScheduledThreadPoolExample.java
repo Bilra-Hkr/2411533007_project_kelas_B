661da36e6e09b1832735006ae59eed5ef517a0ea
Bilra-Hkr <derielchae@gmail.com>
2025-12-16 22:42:18 +0700

    Praktikum_10

---
+++ praktikum_10/ScheduledThreadPoolExample.java
@@ -0,0 +1,30 @@
+package praktikum_10;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.Calendar;
+
+public class ScheduledThreadPoolExample {
+    public static void main(String[] args) {
+        int corePoolSize = 2;
+
+        try (ScheduledThreadPoolExecutor threadPool = new ScheduledThreadPoolExecutor(corePoolSize)) {
+            Runnable task1 = new Command("task1");
+            Runnable task2 = new Command("task2");
+
+            System.out.println("Current time:" + Calendar.getInstance().get(Calendar.SECOND));
+            
+            threadPool.scheduleAtFixedRate(task1, 2, 8, TimeUnit.SECONDS);
+            
+            threadPool.scheduleWithFixedDelay(task2, 5, 5, TimeUnit.SECONDS);
+
+            try {
+                Thread.sleep(30000); 
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            threadPool.shutdown();
+        }
+    }
+}
